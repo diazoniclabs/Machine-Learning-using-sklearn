eb2cb8ac10dba575b48ffdea60ee1f23626cc32b
diazonic <61781809+diazonic@users.noreply.github.com>
2021-02-01 22:43:17 +0530

    Update and rename json_file.py to Saving_models/json_file.py

--- Saving_models/json_file.py
+++ Saving_models/json_file.py
@@ -2,3 +2,6 @@ import json
 values = {'coef':model.coef_[0],'intercept':model.intercept_}
 with open("sample.json", "w") as outfile:  
     json.dump(values, outfile) 
+    
+with open("sample.json", "r") as outfile:  
+    x = json.load(values, outfile)
